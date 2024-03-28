9cc170f44d22e89e3c8b63920802bc067503b54f
chasenpai <wvwv0612@gmail.com>
2024-03-28 20:31:32 +0900

    keepAlive, invalidate, consumer

--- lib/riverpod/code_generation_provider.dart
+++ lib/riverpod/code_generation_provider.dart
@@ -4,7 +4,8 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'code_generation_provider.g.dart';
 
 //어떤 privder를 사용할지 고민할 필요 없고
-//코드 제너레이션을 사용하면 riverpod이 알아서 결정을 해준다
+//코드 제너레이션을 사용하면 riverpod이 알아서 결정을 해줌
+//AutoDispose가 자동으로 걸린다
 final _testProvider = Provider<String>((ref) => 'Code Generation');
 
 @riverpod
@@ -12,6 +13,55 @@ String gState(GStateRef ref) {
   return 'Code Generation';
 }
 
-//family 파라미터를 일반 함수처럼 사용할 수 있게 해준다
+@riverpod
+Future<int> gStateFuture(GStateFutureRef ref) async {
+  await Future.delayed(Duration(seconds: 3));
+  return 10;
+}
 
+//keepAlive - AutoDispose를 사용하지 않도록 설정
+@Riverpod(
+  keepAlive: true,
+)
+Future<int> gStateFutureV2(GStateFutureV2Ref ref) async {
+  await Future.delayed(Duration(seconds: 3));
+  return 10;
+}
+
+
+class Parameter {
+  final int num1;
+  final int num2;
+  Parameter({required this.num1, required this.num2});
+}
+final _testFamilyProvider = Provider.family<int, Parameter>((ref, param)
+  => param.num1 * param.num2
+);
+
+//family 파라미터를 일반 함수처럼 사용할 수 있게 해준다
+@riverpod
+int gStateMultiply(GStateMultiplyRef ref, {
+    required int num1,
+    required int num2
+}) {
+  return num1 * num2;
+}
+
+//StateNotifier
+@riverpod
+class GStateNotifier extends _$GStateNotifier {
+
+  @override
+  int build() {
+    return 0;
+  }
+
+  increment() {
+    state++;
+  }
+
+  decrement() {
+    state--;
+  }
+}
 
--- lib/screen/code_generation_screen.dart
+++ lib/screen/code_generation_screen.dart
@@ -10,14 +10,96 @@ class CodeGenerationScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
 
     final state1 = ref.watch(gStateProvider);
+    final state2 = ref.watch(gStateFutureProvider);
+    final state3 = ref.watch(gStateFutureV2Provider);
+    final state4 = ref.watch(gStateMultiplyProvider(num1: 2, num2: 3));
 
     return DefaultLayout(
       title: 'CodeGenerationScreen',
       body: Column(
         children: [
           Text('state1: $state1'),
+          state2.when(
+            data: (data) => Text(
+              'state2: $data',
+              textAlign: TextAlign.center,
+            ),
+            error: (err, stack) => Text(
+              err.toString(),
+              textAlign: TextAlign.center,
+            ),
+            loading: () => Center(
+                child: CircularProgressIndicator()
+            ),
+          ),
+          state3.when(
+            data: (data) => Text(
+              'state3: $data',
+              textAlign: TextAlign.center,
+            ),
+            error: (err, stack) => Text(
+              err.toString(),
+              textAlign: TextAlign.center,
+            ),
+            loading: () => Center(
+                child: CircularProgressIndicator()
+            ),
+          ),
+          Text('state4: $state4'),
+          //_StateFiveWidget(),
+          Consumer(
+            //상위 빌드가 재실행되지 않고 이 빌더 함수가 재실행 된다
+            builder: (context, ref, child) {
+              final state5 = ref.watch(gStateNotifierProvider);
+              return Row(
+                children: [
+                  Text('state5: $state5'),
+                  child!,
+                ],
+              );
+            },
+            //여기 넣는 child를 빌더 함수의 child로 제공
+            //해당 위젯은 단 한번만 렌더링 된다
+            child: Text(' hello'),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(gStateNotifierProvider.notifier).increment();
+                },
+                child: Text('up'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(gStateNotifierProvider.notifier).decrement();
+                },
+                child: Text('down'),
+              ),
+              //invalidate - state를 초기 상태로 되돌림
+              ElevatedButton(
+                onPressed: () {
+                  ref.invalidate(gStateNotifierProvider);
+                },
+                child: Text('invalidate'),
+              ),
+            ],
+          ),
         ],
       ),
     );
   }
 }
+
+//state가 변경되면 빌드가 다시 실행된다
+//ConsumerWidget으로 감싸서 부분적으로 빌드
+//하지만 코드 템플릿이 많아진다 - Consumer 위젯 제공
+class _StateFiveWidget extends ConsumerWidget {
+  const _StateFiveWidget({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state5 = ref.watch(gStateNotifierProvider);
+    return Text('state5: $state5');
+  }
+}
+
